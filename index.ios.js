4ac3c3a1373dc761b0106a0b9092b9d1ad3014cb
Crazy <crazy@Crazys-Mac.local>
2017-08-18 15:19:51 +0700

    added index.ios.js

---
+++ index.ios.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { AppRegistry, View, StyleSheet } from 'react-native';
+
+import Root from './src/main';
+
+export default class CarTaxi extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Root {...this.props} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+})
+
+AppRegistry.registerComponent('CarTaxi', () => CarTaxi);
