eada8dba9149596238778d82e5216b76aded5508
Crazy <crazy@Crazys-Mac.local>
2017-08-18 15:36:37 +0700

    Change it

--- index.ios.js
+++ index.ios.js
@@ -15,7 +15,9 @@ export default class CarTaxi extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   }
 })
 
